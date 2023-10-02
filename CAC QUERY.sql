5d8d1f30e03094aa5174e65f7354eccd0a340aef
TOSIN-OMOTAYO <109442733+TOSIN-OMOTAYO@users.noreply.github.com>
2023-10-02 22:29:35 +0100

    Add files via upload
    Dataset and Query to evaluate a campaign by CAC.

---
+++ CAC QUERY.sql
@@ -0,0 +1,23 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+-- CAC PER CHANNEL
+
+Select Marketing_Channel, Sum(CAC) as Total_CAC
+from
+(
+SELECT *,Marketing_Spend/New_Customers as CAC
+FROM [NEW DATABASE].[dbo].customer_acquisition_cost_dataset)X
+  
+Group by Marketing_Channel
+
+
+
+--BREAK EVEN CUSTOMERS
+SELECT Marketing_Channel, Sum(Break_even_Customers) as Total_Break_even_Customers
+from(
+SELECT Marketing_Channel, Marketing_Spend/CAC as Break_even_Customers
+from
+(
+ SELECT *,Marketing_Spend/New_Customers as CAC
+FROM [NEW DATABASE].[dbo].customer_acquisition_cost_dataset)x
+)y
+Group by Marketing_Channel
